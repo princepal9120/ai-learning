894f838f14300ba889a7171ed87b4475d8bafae0
Prince Pal <princepal9120@gmail.com>
2025-06-23 10:16:32 +0000

    Implement news audio generation endpoint and add scraper files

---
+++ mcp/mcp_journlist/backend.py
@@ -0,0 +1,30 @@
+from models import NewsRequests
+from fastapi import FastApi
+form 
+
+
+app =FastApi()
+
+@app.post("/generate-news-audio")
+async def generate_news_audio(requests: NewsRequests):
+    try:
+        results={}
+        
+        #Scrape data
+
+        if requests.source_type in ["news" "both"]:
+            #scrape news
+            results["news"] = {"news_scrapped": "this is from google newss"}
+        
+        if requests.source_type in ["reddit","both"]:
+            #scrape reddit
+            results["reddit"] ={"reddit_scrapped": "This is from reddit"}
+        
+        news_data= results.get("news", {})
+        reddit_data=results.get("reddit",{})
+
+        #setup LLM summarizer
+
+        news_summary=my_summary_function(news_data, reddit_data)
+
+        audio_path=convert_text_to_audio(news_summary)
