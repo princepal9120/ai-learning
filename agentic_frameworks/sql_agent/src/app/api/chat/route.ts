4f5c9f906cf44667933b5dd2f98b400c76881e0b
Prince Pal <princepal9120@gmail.com>
2025-10-21 22:40:43 +0530

    add tool calling

--- agentic_frameworks/sql_agent/src/app/api/chat/route.ts
+++ agentic_frameworks/sql_agent/src/app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { openai } from '@ai-sdk/openai';
-import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { streamText, UIMessage, convertToModelMessages, tool } from 'ai';
+import z from 'zod';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -10,6 +11,21 @@ export async function POST(req: Request) {
   const result = streamText({
     model: openai('gpt-4o'),
     messages: convertToModelMessages(messages),
+     tools: {
+      weather: tool({
+        description: 'Get the weather in a location (fahrenheit)',
+        inputSchema: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+    },
   });
 
   return result.toUIMessageStreamResponse();
